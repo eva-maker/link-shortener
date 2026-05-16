7cbc254bf6d18af8541f26bdd62393a7d49bdaf1
Yeva Bykova <ieva.bykova@gmail.com>
2026-05-16 20:24:45 +0300

    Add edit, delete, change password pages and update UI

--- shortener/urls.py
+++ shortener/urls.py
@@ -5,22 +5,15 @@ from links import views as links_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # прості сторінки
     path('', links_views.home, name='home'),
     path('about/', links_views.about, name='about'),
-
-    # реєстрація / логін / логаут
     path('register/', links_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(
-        template_name='login.html'
-    ), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', links_views.user_logout, name='logout'),
-
-    # особистий кабінет + посилання
     path('profile/', links_views.profile, name='profile'),
+    path('change-password/', links_views.change_password, name='change_password'),
     path('links/', links_views.links_page, name='links'),
-
-
+    path('links/<int:pk>/edit/', links_views.edit_link, name='edit_link'),
+    path('links/<int:pk>/delete/', links_views.delete_link, name='delete_link'),
     path('s/<str:code>/', links_views.redirect_short, name='redirect_short'),
 ]
